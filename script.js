514e0550eeabe6eb371cf9642935b260b005d085
RenzoWille <renzo.wille@hva.nl>
2024-11-21 20:28:38 +0100

    fixes #3

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").addEventListener("click", function() {
+    var menu = document.getElementById("mobile-menu");
+    menu.classList.toggle("active"); // Toggle the 'active' class to show/hide the menu
+});
